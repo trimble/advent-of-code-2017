ea312ead860f41927daea3d57ed07be0da854cdb
Todd Trimble <trimble@sep.com>
2017-12-09 18:06:00 -0500

    Day 9 pt 2

--- 9/garbage.py
+++ 9/garbage.py
@@ -1,26 +1,11 @@
 import re
 
-def process(x):
-    temp = re.sub(r'{.*}', 'x', content)
-    return content.count('x')
-
 content = (open('input.txt').readlines())[0]
 
 content = content.strip()
 content = re.sub(r'!.', "", content)
+print(len(content))
+x = re.findall(r'(<.*?>)', content)
+print(len(x))
 content = re.sub(r'<.*?>', "", content)
-content = re.sub(r',', "", content)
-content = re.sub(r'}{', "},{", content)
-
-count = 1
-score = 0
-for i in content:
-    if i == '{':
-        score += count
-        count += 1
-    elif i == '}':
-        count -= 1
-
-print(content)
-print(count)
-print(score)
+print(len(content))
