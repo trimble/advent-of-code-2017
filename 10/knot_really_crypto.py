96c7f39cc293c5e6089b624251a4c71e5140b660
Todd Trimble <trimble@sep.com>
2017-12-10 22:24:11 -0500

    Day 10 pt 2 (was mostly an excercise in decoding the poorly written instructions)

--- 10/knot_really_crypto.py
+++ 10/knot_really_crypto.py
@@ -1,7 +1,10 @@
 state = list(range(0,256))
-input = [199,0,255,136,174,254,227,16,51,85,1,2,22,17,7,192]
+input = '199,0,255,136,174,254,227,16,51,85,1,2,22,17,7,192'
 #state = list(range(0,5))
 #input = [3, 4, 1, 5]
+
+input = [ord(i) for i in input]
+input = input + [17, 31, 73, 47, 23]
 
 def process(state, length, skip):
     copy = state
@@ -16,15 +19,18 @@ def rotate(myList, index):
 
 current = 0
 skip = 0
-for i in input:
-    temp = rotate(state, current)
-    print(f"temp={temp}")
-    process(temp, i, skip)
-    print(f"processed={temp}")
-    state = rotate(temp, len(state)-current)
-    print(f"state={state}")
-    current = (current + (i + skip)) % len(state)
-    print(current)
-    skip += 1
+for j in list(range(64)):
+    for i in input:
+        temp = rotate(state, current)
+        process(temp, i, skip)
+        state = rotate(temp, len(state)-current)
+        current = (current + (i + skip)) % len(state)
+        skip += 1
 print(state)
-print(state[0]*state[1])
+dense = []
+for i in list(range(16)):
+    temp = state[16*i]
+    for j in list(range(1,16)):
+        temp ^= state[16*i+j] 
+    dense.append(hex(temp))
+print(dense)
