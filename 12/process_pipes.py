0fe0baa6ca19854d687041e46c813f8681b4f2fb
Todd Trimble <trimble@sep.com>
2017-12-12 12:50:17 -0500

    Pull out junk prints

--- 12/process_pipes.py
+++ 12/process_pipes.py
@@ -7,22 +7,11 @@ content = [x.strip() for x in content]
 
 state = {}
 for line in content:
-    print(line)
     x = re.search(r'(.*?) <-> (.*)', line)
-    print("process {} connects to {}".format(x.group(1), x.group(2)))
     state[str(x.group(1))] = x.group(2).split(', ')
-
-print(state)
 
 graph = Graph(state)
 
-print("Vertices of graph:")
-print(graph.vertices())
-
-print("Edges of graph:")
-print(graph.edges())
-
-print(graph.find_path('0', '3'))
 count = 0
 for i in graph.vertices():
     if graph.find_path(i, '0'):
