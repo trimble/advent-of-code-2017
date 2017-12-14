720face3b45904676fea57c0d1d238442d265997
Todd Trimble <trimble@sep.com>
2017-12-13 23:04:00 -0500

    Day 13 pt 2

--- 13/solve.py
+++ 13/solve.py
@@ -1,6 +1,6 @@
 import re
 
-with open('input.txt') as f:
+with open('test.txt') as f:
     content = f.readlines()
 content = [x.strip() for x in content]
 
@@ -9,16 +9,18 @@ for i in content:
     x = re.search(r'(.*?): (.*)', i)
     board[str(x.group(1))] = int(x.group(2))
 
-print(board)
+caught = True
+delay = 0
+while caught:
+    caught = False 
+    for i in range(97):
+        step = i + delay
+        depth = board.get(str(i))
+        if depth:
+            if (step % (2*depth-2) == 0):
+                print("delay = {}, caught in layer {}".format(delay, i))
+                caught = True
+                break
+    delay += 1
 
-score = 0
-for i in range(97):
-    depth = board.get(str(i))
-    print("i = {}, depth = {}".format(i, depth))
-    if depth:
-        print(i % (2*depth-2))
-        if (i % (2*depth-2) == 0):
-            print("caught in layer {}".format(i))
-            score += (i * depth)
-
-print(score)
+print(delay)
