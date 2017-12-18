8fdf757798c774419042672840f81a5733451875
Todd Trimble <trimble@sep.com>
2017-12-17 22:44:55 -0500

    Day 17 pt 2 (was more math than anything)

--- 17/spin.py
+++ 17/spin.py
@@ -2,15 +2,9 @@ state = [0]
 input = 337
 current = 0
 
-for i in range(1,50):
-    temp = current
-    state = state[current:] + state[:current]
-    current = 0
-    pos = input % len(state)
-    state.insert(pos+1, i)
-    state = state[state.index(0):] + state[:state.index(0)]
-    current = state.index(i)
-    print(state)
+for i in range(1, 50000001):
+    current = ((current + input) % i) + 1
+    if current == 1:
+        last_1 = i
 
-print(state.index(2017))
-print(state[state.index(2017)-30:state.index(0)+3])
+print(last_1)
