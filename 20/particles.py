3ecc2e6c2b897a3a161ce42eb39da265937be165
Todd Trimble <trimble@sep.com>
2017-12-20 12:47:03 -0500

    Rework using dict

--- 20/particles.py
+++ 20/particles.py
@@ -3,34 +3,34 @@ import re
 def distance(position):
     return abs(position[0]) + abs(position[1]) + abs(position[2])
 
-with open('input.txt') as f:
+with open('test.txt') as f:
     content = f.readlines()
 content = [x.strip() for x in content]
 
-particles = []
+particles = {}
 
 for name, line in enumerate(content):
     x = re.match( r'p=<(.*?)>, v=<(.*?)>, a=<(.*?)>$', line)
     position = [int(i) for i in x.group(1).split(',')]
     velocity = [int(i) for i in x.group(2).split(',')]
     accel = [int(i) for i in x.group(3).split(',')]
-    particles.append({
+    particles[name] = {
       'position': position,
       'velocity': velocity,
       'acceleration': accel,
       'distance': distance(position),
-      'name': name,
-    })
+    }
 
-for j in range(10000):
-    for i in particles:
-        i['velocity'][0] += i['acceleration'][0]
-        i['velocity'][1] += i['acceleration'][1]
-        i['velocity'][2] += i['acceleration'][2]
-        i['position'][0] += i['velocity'][0]
-        i['position'][1] += i['velocity'][1]
-        i['position'][2] += i['velocity'][2]
-        i['distance'] = distance(i['position'])
+for j in range(3):
+    positions = {}
+    for k, v in particles.items():
+        v['velocity'][0] += v['acceleration'][0]
+        v['velocity'][1] += v['acceleration'][1]
+        v['velocity'][2] += v['acceleration'][2]
+        v['position'][0] += v['velocity'][0]
+        v['position'][1] += v['velocity'][1]
+        v['position'][2] += v['velocity'][2]
+        v['distance'] = distance(v['position'])
+        positions.setdefault(str(v['position']), []).append(k)
 
-particles.sort(key=lambda x: x['distance'])
-print(particles[0]['name'])
+print(particles)
