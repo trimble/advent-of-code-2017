bf605a8ab83d4989a49ce74fc61532d1844ec04d
Todd Trimble <trimble@sep.com>
2017-12-22 21:35:00 -0500

    Day 22 pt 1 (tweaked to allow expansion of states)

--- 22/virus.py
+++ 22/virus.py
@@ -8,26 +8,26 @@ grid=[]
 for i in range(1000):
     x = []
     for j in range(1000):
-        x.append(False)
+        x.append('C')
     grid.append(x)
 
 for index, i in enumerate(content):
     for k, j in enumerate(i):
         if j == '#':
-            grid[index+500][k+500] = True
+            grid[index+500][k+500] = 'I'
 
 position = [512, 512]
 direction = [-1, 0]
 
 count = 0
 for i in range(10000):
-    if grid[position[0]][position[1]]:
-        grid[position[0]][position[1]] = False
+    if grid[position[0]][position[1]] == 'I':
+        grid[position[0]][position[1]] = 'C'
     else:
-        grid[position[0]][position[1]] = True
+        grid[position[0]][position[1]] = 'I'
         count += 1
 
-    if grid[position[0]][position[1]]:
+    if grid[position[0]][position[1]] == 'I':
         if direction == [-1, 0]:
             direction = [0, -1]
         elif direction == [0, -1]:
